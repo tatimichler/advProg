5e084b565330cc45f233b25e002e35d3f3469e61
laeberkaes <50055905+laeberkaes@users.noreply.github.com>
2023-06-28 19:53:51 +0200

    script stuff

--- sniffsniff.py
+++ sniffsniff.py
@@ -6,8 +6,8 @@ from scapy.all import *
 from scapy.layers.dot11 import Dot11
 
 class Sniffer():
-    def __init__(self, iface=os.getenv("IFACE"), threshold=250, time_delta=30.0, ap_list=["B8:27:EB:AC:05:0F"],target_queue=[None], attack_uuid=None, log_path="/opt/sniff/logedi.log"):
-    # def __init__(self, iface="wlan0mon", threshold=250, time_delta=30.0, ap_list=["B8:27:EB:AC:05:0F"],target_queue=[None], attack_uuid=None, log_path="/opt/sniff/logedi.log"):
+    #def __init__(self, iface=os.getenv("IFACE"), threshold=250, time_delta=30.0, ap_list=["B8:27:EB:AC:05:0F"],target_queue=[None], attack_uuid=None, log_path="/opt/sniff/logedi.log"):
+    def __init__(self, iface="wlan0", threshold=2, time_delta=30.0, ap_list=["B8:27:EB:AC:05:0F", "30:4B:07:ED:E3:8F"],target_queue=[None], attack_uuid=None, log_path="/opt/sniff/logedi.log"):
         """
         Constructor of deauth attack sniffer.
 
@@ -28,8 +28,9 @@ class Sniffer():
         self.log_path = log_path
 
         os.system("airmon-ng check kill")
-        os.system(f"airmon-ng start {os.getenv('IFACE_PRE')}")
-        # os.system(f"airmon-ng start wlan0")
+        #os.system(f"airmon-ng start {os.getenv('IFACE_PRE')}")
+        os.system(f"airmon-ng start wlan0")
+        os.system(f"airodump-ng wlan0 -c 1 > /dev/null &")
         os.system(f"service filebeat start")
 
     def write_frame_to_file(self, buffer_item: dict):
@@ -104,7 +105,7 @@ class Sniffer():
         if recieved_frame.haslayer(Dot11):
             #self.debug(recieved_frame)
             if recieved_frame.type == 0 and recieved_frame.subtype == 12:
-                self.debug(recieved_frame)
+                # self.debug(recieved_frame)
                 self.check_for_threshold(recieved_frame)
 
     def debug(self, recieved_frame):
